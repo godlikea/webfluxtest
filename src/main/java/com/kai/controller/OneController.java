4602a9929e47631a591279160557e4bef3e10c25
guoguangkai <guoguangkai@example.com>
2019-03-27 09:05:47 +0800

    添加测试

---
+++ src/main/java/com/kai/controller/OneController.java
@@ -0,0 +1,20 @@
+package com.kai.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+/**
+ * 第一个响应式
+ * @author ggk
+ * @data 2019年3月27日上午8:55:53
+ */
+@RestController
+@RequestMapping("/mono")
+public class OneController {
+
+	@RequestMapping("/getlook")
+	public Mono<String> getHelloWord(){
+		return Mono.just("this is look");
+	}
+}
